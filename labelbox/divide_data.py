eb1bd2bd9f928d98337d7d63faa946b82e3d9a27
Niida Mai <n-maimai@MacBook-Pro.local>
2020-05-14 15:06:11 +0900

    解決したのでデバッグのみ

--- labelbox/divide_data.py
+++ labelbox/divide_data.py
@@ -7,29 +7,19 @@ img_path = "dummy/image/"
 # 読み込むファイルのパス
 path = "csv/export.csv"
 
-with open(path) as file:
+with open(path, 'r') as file:
     # リスト形式
-    f = csv.reader(file, delimiter=",", doublequote=True, lineterminator="\n", quotechar='"', skipinitialspace=True)
-    header = next(f)
+    f = csv.reader(file, delimiter=',', quotechar='"')
+    # ヘッダーはスキップ
+    next(f)
     a = 0
     for col in f:
-        print(f)
+        print(col)
         a = a + 1
         if a == 10:
             break
 
 
 '''
-１０行だけデバッグしてみたが、結果は以下
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-<_csv.reader object at 0x100c0a978>
-想定は、カンマ区切りのデータ
+デバッグ中
 '''
