3acdc53a8b25ea6df37a0fd035ba67706b074036
Niida Mai <n-maimai@MacBook-Pro.local>
2020-05-15 15:02:21 +0900

    データ分けのテキストファイルを出力（暫定）

--- labelbox/divide_data.py
+++ labelbox/divide_data.py
@@ -4,20 +4,27 @@ import csv
 # イメージパス
 img_path = "dummy/image/"
 
-# 読み込むファイルのパス
+# 読み込みファイルのパス
 path = "csv/export.csv"
 
-with open(path, 'r') as file:
-    # リスト形式
-    f = csv.reader(file, delimiter=',', quotechar='"')
+# テストファイルのパス
+test_path = "test.txt"
+# 検証ファイルのパス
+valid_path = "validation.txt"
+# 訓練ファイルのパス
+train_path = "train.txt"
+
+with open(path, 'r') as f:
+    # リスト形式に読み込む
+    l = csv.reader(f, delimiter=',', quotechar='"')
 
     # ヘッダーはスキップ
-    next(f)
+    next(l)
 
     test_list = []
     valid_list = []
     train_list = []
-    for i, col in enumerate(f):
+    for i, col in enumerate(l):
         fp = img_path + col[9]
         c = i % 3
         if  c == 1 and len(test_list) < 100:
@@ -27,10 +34,14 @@ with open(path, 'r') as file:
         else:
             train_list.append(fp)
 
-print(test_list)
-print(valid_list)
-print(train_list)
+# テストファイル書き込み
+with open(test_path, 'w') as f:
+    f.write('\n'.join(test_list))
 
-'''
-デバッグ中
-'''
+# 検証ファイル書き込み
+with open(valid_path, 'w') as f:
+    f.write('\n'.join(valid_list))
+
+# 訓練ファイル書き込み
+with open(train_path, 'w') as f:
+    f.write('\n'.join(train_list))
