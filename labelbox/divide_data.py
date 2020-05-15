280181fc6f8426ebfe1f5474e3f2f149a78a958e
Niida Mai <n-maimai@MacBook-Pro.local>
2020-05-15 12:10:17 +0900

    データを分ける処理を追加

--- labelbox/divide_data.py
+++ labelbox/divide_data.py
@@ -14,11 +14,22 @@ with open(path, 'r') as file:
     # ヘッダーはスキップ
     next(f)
 
-    name = []
-    for col in f:
-        name.append(img_path + col[9])
+    test_list = []
+    valid_list = []
+    train_list = []
+    for i, col in enumerate(f):
+        fp = img_path + col[9]
+        c = i % 3
+        if  c == 1 and len(test_list) < 100:
+            test_list.append(fp)
+        elif c == 2 and len(valid_list) < 100:
+            valid_list.append(fp)
+        else:
+            train_list.append(fp)
 
-print(name)
+print(test_list)
+print(valid_list)
+print(train_list)
 
 '''
 デバッグ中
